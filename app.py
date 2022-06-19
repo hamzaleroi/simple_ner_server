e0538623049d46f0d3f590d464993d8782eee24b
Hamza Benyamina <hamzabenyamina@Hamza-Macbook-Air.local>
2022-06-19 19:08:35 +0400

    added docs and formatted code

--- app.py
+++ app.py
@@ -1,32 +1,52 @@
-from fastapi import FastAPI
+import json
+import time
+from typing import Dict
+
 import mysql.connector
 import spacy
 import uvicorn
-from db_utils.crud import log_ners, get_ner_logs
-import json
-import time
+from fastapi import FastAPI
+
+from db_utils.crud import get_ner_logs, log_ners
 
 app = FastAPI(host="0.0.0.0", port=8000, reload=True)
 nlp = spacy.load("en_core_web_md")
 
 
-
-
 @app.get("/ner_extract/{query}")
-async def root(query:str):
-    conn = mysql.connector.connect(user='user', password='password', host='db', database='logs')
+async def root(query: str) -> Dict:
+    """
+    Extract named entities from a text.
+    query: The text from which to extract named entities.
+
+    - Returns a dictionary of word and its equivalent named entity.
+    """
+    conn = mysql.connector.connect(
+        user="user", password="password", host="db", database="logs"
+    )
     start_time = time.time()
     doc = nlp(query)
-    ners = dict( [(item.text, item.label_) for item in doc.ents])
-    log_list = (query,json.dumps(ners),time.time() - start_time,)
-    log_ners(conn,ners=log_list)
+    ners = dict([(item.text, item.label_) for item in doc.ents])
+    log_list = (
+        query,
+        json.dumps(ners),
+        time.time() - start_time,
+    )
+    log_ners(conn, ners=log_list)
     conn.close()
     return ners
 
+
 @app.get("/logs/{limit}")
-async def root(limit:int=1):
+async def root(limit: int = 1):
+    """
+    Fetches logs from the database.
+    limit: The number of log items to fetch.
+
+    - Returns a list of logged items.s
+    """
     conn = mysql.connector.connect(
-    user='user', password='password', host='db', database='logs'
+        user="user", password="password", host="db", database="logs"
     )
     results = get_ner_logs(conn, limit=limit)
     conn.close()
--- db_utils/crud.py
+++ db_utils/crud.py
@@ -1,21 +1,27 @@
+import json
+from typing import Tuple
 
 import mysql.connector
-from typing import Tuple
-import json
 
-def log_ners(conn,ners: Tuple):
+
+def log_ners(conn, ners: Tuple):
     sql = "INSERT INTO logs (request_query, ners, execution_time) VALUES (%s, %s, %s);"
     with conn.cursor() as cursor:
         cursor.execute(sql, ners)
         conn.commit()
 
+
 def get_ner_logs(conn, limit: int):
     sql = f"SELECT * from logs LIMIT {limit}"
     with conn.cursor() as cursor:
         cursor.execute(sql)
-        results = cursor.fetchall() 
-    return [{"query":result[1],
-        "named_entities":json.loads(result[2]),
-        "execution_time":result[3],
-        "timestamp":result[4]}
-         for result in results]
+        results = cursor.fetchall()
+    return [
+        {
+            "query": result[1],
+            "named_entities": json.loads(result[2]),
+            "execution_time": result[3],
+            "timestamp": result[4],
+        }
+        for result in results
+    ]
